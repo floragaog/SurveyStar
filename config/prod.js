24f9bd309552f12355157bd28e38dd6492f0f482
高歌 <flora@MacBook-Air.local>
2021-07-01 13:47:59 -0700

    separated dev/prod

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports= {
+    googleClientID : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI : process.env.MONGO_URI,
+    cookieKey : process.env.COOKIE_KEY,
+}
+//mongodb+srv://myuser:CE6e8agNI5xwE5zr@cluster0.kjj1e.mongodb.net/test?retryWrites=true&w=majority
